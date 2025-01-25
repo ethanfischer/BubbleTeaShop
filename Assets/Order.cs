c62051eccea93897bce1dda10c918dd4072cb34d
ethanfischer <ethan.fischer@incontextsolutions.com>
2025-01-24 22:49:56 -0600

    drive values from code

--- Assets/Order.cs
+++ Assets/Order.cs
@@ -9,6 +9,24 @@ public class Order : MonoBehaviour
     public TMP_Text ToppingsText;
     public TMP_Text SugarText;
     public TMP_Text IceText;
+
+    string _size;
+    string _toppings;
+    string _sugar;
+    string _ice;
+    
+    public void Initialize(string size, string toppings, string sugar, string ice)
+    {
+        _size = size;
+        _toppings = toppings;
+        _sugar = sugar;
+        _ice = ice;
+
+        SizeText.text = $"Size: {_size}";
+        ToppingsText.text = $"Toppings: {_toppings}";
+        SugarText.text = $"Sugar: {_sugar}";
+        IceText.text = $"Ice: {_ice}";
+    }
     
     // Start is called before the first frame update
     void Start()
--- Assets/OrderSystem.cs
+++ Assets/OrderSystem.cs
@@ -1,14 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class OrderSystem : MonoBehaviour
 {
-    public List<Order> Orders;
-
-    // Update is called once per frame
+    [SerializeField]
+    Order _orderPrefab;
+    
+    void Start()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            var order = Instantiate(_orderPrefab, this.transform);
+            order.Initialize("Small", "Boba", "30%", "Normal");
+        }
+    }
+    
     void Update()
     {
-        
     }
 }
